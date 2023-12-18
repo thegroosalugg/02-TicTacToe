b9972422ff9f8cb1cfbe10994b4a6bf6984f41a2
victor loginov <tkoe88@zoho.com>
2023-12-18 21:18:03 +0100

    created first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <div id="game-container">
+      <ol id="players">
+        <Player name='Bozo' symbol='X' />
+        <Player name='Jojo' symbol='X' />
+      </ol>
+      Game boooooard
+    </div>
+  );
 }
 
-export default App
+export default App;
