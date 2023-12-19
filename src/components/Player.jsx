280e95b1ca369188c01bcb82d3549964b988a37a
victor loginov <tkoe88@zoho.com>
2023-12-20 00:52:28 +0100

    updated component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [edit, setEdit] = useState(false);
+
+  function editName() {
+    setEdit(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (edit) {
+    playerName = <input type='text' required/>;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {/* <span className="player-name">{name}</span> */}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={editName}>Edit</button>
     </li>
   );
 }
