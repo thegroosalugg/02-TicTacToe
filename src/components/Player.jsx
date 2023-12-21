680a9b6adc44cc376e2308ef0a567e1e098ea664
victor loginov <tkoe88@zoho.com>
2023-12-21 19:42:52 +0100

    updated notes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,18 +5,18 @@ export default function Player({ initialName, symbol }) {
   const [playerName, setPlayerName] = useState(initialName)
 
   function handleClick() {
-    setEdit((toggle) => !toggle);
-  }
+    setEdit((toggle) => !toggle); // the arrow function reverses the current state, i.e. change the boolean true to false
+  }                               // the arrow function name is irrelevant and serve only as a parameter
 
   function handleChange(event) {
-    setPlayerName(event.target.value);
+    setPlayerName(event.target.value); // event.target.value will save the value of the input in useState playerName
   }
 
   let editPlayerName = <span className="player-name">{playerName}</span>;
 
   if (editting) {
     editPlayerName = <input type='text' required value={playerName} onChange={handleChange} />;
-  }
+  } // output value must be stored inside input (playerName), onChange is an event listener which calls a function
 
   return (
     <li>
