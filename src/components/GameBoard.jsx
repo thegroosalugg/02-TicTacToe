91c15a8e0498eed6c83e16270538a5e3b8c29f39
victor loginov <tkoe88@zoho.com>
2023-12-24 12:43:01 +0100

    added button function to gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,27 @@ const initialBoard = [
 ];
 
 export default function GameBoard() {
+  // each row is an array. Each 'column' is a single element inside the array, and not an actual column
+  const [gameBoard, setGameboard] = useState(initialBoard);
+
+  function handleBoard(rowIndex, colIndex) {
+    setGameboard((prevGameBoard) => {
+      const updateBoard = [ // new inmutable array is created using ...spread operator as to not alter original
+        ...prevGameBoard.map((innerArray) => [...innerArray]), // 
+      ];
+      updateBoard[rowIndex][colIndex] = "X";
+      return updateBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((col, colIndex) => (
+            {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button></button>
+                <button onClick={() => handleBoard(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
